9781a721b9dcd2551c5fff38811d4f0eba24277e
Wesley Soo-Hoo <wesoohoo@gmail.com>
2021-02-09 09:59:06 -0800

    insert optimization notes

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -6,6 +6,12 @@ void print_x() {
     printf("%i", x);
 }
 
+// Notes about optimization: With optimization enabled, the compiler only brings
+// in variables necessary to the program output. For example, with line 15
+// uncommented, it is necessary to compute the value of `y`, so the program
+// needs `x` and `y` in memory. However, when that line is commented out, `y` is
+// no longer needed (it's an unreferenced variable), so it is omitted.
+
 int main() {
     int y = x + 1;
     printf("Hello, World!\n");
